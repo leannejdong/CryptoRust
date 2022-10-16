3bdf9fcc906f695e37b5bb3a399b3075c1690c62
Leanne J Dong <jdleanne@gmail.com>
2022-10-17 02:56:18 +1100

    add hmac

--- src/lib.rs
+++ src/lib.rs
@@ -3,6 +3,7 @@ extern crate sha2;
 // use crypto::sha2::Sha256;
 use hex_literal::hex;
 use sha2::{Sha256, Sha512, Digest};
+use hmac::{Hmac, Mac};
 // use near_sdk::{env};
 
 fn cal_hash(input:&'static [u8; 11]){
@@ -11,6 +12,18 @@ fn cal_hash(input:&'static [u8; 11]){
     //println!("{}", sha.result_str());
     let result = sha.finalize();
     format!("{:x}", result);
+}
+
+fn cal_hmac(input:&'static [u8]){
+    type HmacSha256 = Hmac<Sha256>;
+    let mut mac = HmacSha256::new_from_slice(b"my secret and secure key")
+    .expect("HMAC can take key of any size");
+mac.update(b"input message");
+// `result` has type `CtOutput` which is a thin wrapper around array of
+// bytes for providing constant time equality check
+    let result = mac.finalize();
+    //println!("{}", result);
+
 }
 
 #[cfg(test)]
@@ -30,6 +43,11 @@ mod tests{
         // assert_eq!(result, "Expected the correct answer to return true.");
             println!("Let's debug:{:?}", result);
 
+    }
+    #[test]
+    fn test_cal_hmac(){
+        let result = cal_hmac(b"hello world");
+        println!("Let's debug:{:?}", result);
     }
 
     #[test]
@@ -66,6 +84,28 @@ mod tests{
         989dd35bc5ff499670da34255b45b0cfd830e81f605dcf7dc5542e93ae9cd76f
     ")[..]);
 
+    }
+    #[test]
+    //#[should_panic]
+    fn test_hmac(){
+        // Create alias for HMAC-SHA256
+    type HmacSha256 = Hmac<Sha256>;
 
+    let mut mac = HmacSha256::new_from_slice(b"my secret and secure key")
+        .expect("HMAC can take key of any size");
+    mac.update(b"input message");
+
+    // `result` has type `CtOutput` which is a thin wrapper around array of
+    // bytes for providing constant time equality check
+    let result = mac.finalize();
+    // To get underlying array use `into_bytes`, but be careful, since
+    // incorrect use of the code value may permit timing attacks which defeats
+    // the security provided by the `CtOutput`
+    let code_bytes = result.into_bytes();
+    let expected = hex!("
+        97d2a569059bbcd8ead4444ff99071f4
+        c01d005bcefe0d3567e1be628e5fdcd9
+    ");
+    assert_eq!(code_bytes[..], expected[..]);
     }
 }
